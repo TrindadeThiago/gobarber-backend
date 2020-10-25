fc7229a0f06006f37a573f4191dcc9d23545067c
Thiago Trindade <thiago.trindadexd@gmail.com>
2020-10-25 16:11:53 -0300

    criando agendamento

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.get("/", (request, response) => response.json({ message: "Hello World" }));
-
 app.listen(3333, () => {
   console.log("🚀 Server started on port 3333");
 });
