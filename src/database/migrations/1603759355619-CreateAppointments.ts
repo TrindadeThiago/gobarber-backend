e132fbfe990eebb275b5352caae673c4f85d1e73
Thiago Trindade <thiago.trindadexd@gmail.com>
2020-10-26 21:56:47 -0300

    configuracao typeorm e database e criacao de migration CreateAppointments

---
+++ src/database/migrations/1603759355619-CreateAppointments.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { uuid } from "uuidv4";
+
+export default class CreateAppointments1603759355619
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "appointments",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+            generationStrategy: "uuid",
+          },
+          {
+            name: "provider",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "date",
+            type: "timestamp with time zone",
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("appointments");
+  }
+}
