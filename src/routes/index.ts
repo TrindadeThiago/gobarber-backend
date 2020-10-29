4a3dd2ed2e5cf1b49ae0015c4e4998e51feaa4ef
Thiago Trindade <thiago.trindadexd@gmail.com>
2020-10-28 22:00:23 -0300

    service create user, route users e fix user model e appointment stack

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import appointmentsRouter from "./appointments.routes";
+import usersRoutes from "./users.routes";
 
 const routes = Router();
 
 routes.use("/appointments", appointmentsRouter);
+routes.use("/users", usersRoutes);
 
 export default routes;
