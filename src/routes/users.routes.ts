4eba3f3c283627af0945ccd62f0dd2bf5da46ebd
Thiago Trindade <thiago.trindadexd@gmail.com>
2020-11-02 16:20:09 -0300

    upload de arquivos e update de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
+
+import uploadConfig from "../config/upload";
 
 import CreateUserService from "../services/CreateUserService";
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
+
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const usersRouters = Router();
+const upload = multer(uploadConfig);
 
 usersRouters.post("/", async (request, response) => {
   try {
@@ -23,5 +30,27 @@ usersRouters.post("/", async (request, response) => {
     return response.status(400).json({ message: err.message });
   }
 });
+
+usersRouters.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouters;
