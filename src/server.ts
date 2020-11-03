e4a1f460f305a46b0971a0f0d66e934fb828b5f5
Thiago Trindade <thiago.trindadexd@gmail.com>
2020-11-02 21:14:47 -0300

    fix importacoes server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import "express-async-errors";
 
 import routes from "./routes";
